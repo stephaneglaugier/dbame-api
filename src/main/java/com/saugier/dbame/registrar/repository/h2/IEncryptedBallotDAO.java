a008e6a5b0ae13a8a856a397fed18f9d63329492
Stephane Augier <stephane@augiermail.com>
2023-03-01 13:56:38 +0000

    make it work and delete moderator db

--- src/main/java/com/saugier/dbame/registrar/repository/h2/IEncryptedBallotDAO.java
+++
@@ -1,9 +0,0 @@
-package com.saugier.dbame.registrar.repository.h2;
-
-import com.saugier.dbame.registrar.model.entity.h2.ModeratorRelayME;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEncryptedBallotDAO extends CrudRepository<ModeratorRelayME, Long> {
-}
